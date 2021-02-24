57df5ba49f2c4e39b8d397a8a617391be48fc376
tusharpatil-coditas <67581290+tusharpatil-coditas@users.noreply.github.com>
2021-02-24 23:22:29 +0530

    Add files via upload

---
+++ formula.py
@@ -0,0 +1,10 @@
+class methodology():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def addition(self):
+        return self.a + self.b
+    def substract(self):
+        return self.a - self.b
+    def multiply(self):
+        return self.a * self.b    
